c06ff7f083510bb552aca193c0e1aad7588f047b
animeshburnwal <129171724+animeshburnwal@users.noreply.github.com>
2026-07-08 13:41:21 +0530

    changes

--- Microservices/Hrms/QubeFin.Hrms.Api/Program.cs
+++ Microservices/Hrms/QubeFin.Hrms.Api/Program.cs
@@ -20,7 +20,6 @@ builder.Services.AddEndpoints(typeof(Program).Assembly);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
