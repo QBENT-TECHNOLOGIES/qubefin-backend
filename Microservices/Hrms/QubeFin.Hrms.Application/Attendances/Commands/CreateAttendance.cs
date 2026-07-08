4d7a88f07ba7a298e6aafc69dbd1441e9dfd55e2
animeshburnwal <129171724+animeshburnwal@users.noreply.github.com>
2026-07-08 11:26:44 +0530

    change

---
+++ Microservices/Hrms/QubeFin.Hrms.Application/Attendances/Commands/CreateAttendance.cs
@@ -0,0 +1,43 @@
+﻿using FluentResults;
+using MediatR;
+using QubeFin.Hrms.Persistence.Repositories;
+using QubeFin.Persistence;
+using QubeFin.Persistence.Models.Hrms;
+
+namespace QubeFin.Hrms.Application.Attendances.Commands
+{
+
+    #region --- COMMAND ---
+    public record CreateAttendanceCommand(Guid EmployeeId, TimeOnly time) : IRequest<Result<CreateAttendanceResponse>>;
+    #endregion
+
+    #region --- RESPONSE ---
+    public record CreateAttendanceResponse(bool Created);
+    #endregion
+
+    #region --- HANDLER ---
+    internal sealed class CreateAttendanceCommandHandler(IAttendanceRepository attendanceRepository, IUnitOfWork unitOfWork)
+        : IRequestHandler<CreateAttendanceCommand, Result<CreateAttendanceResponse>>
+    {
+        public async Task<Result<CreateAttendanceResponse>> Handle(CreateAttendanceCommand request, CancellationToken cancellationToken)
+        {
+            var todayAttendance = attendanceRepository.GetTodayAttendanceData(request.EmployeeId);
+
+            if (todayAttendance is null)
+            {
+               
+                var attendance = Attendance.MarkCheckIn(Guid.NewGuid(), request.EmployeeId, request.time, null, null, null);
+            }
+            else
+            {
+                //var attendance = Attendance.Update(Guid.NewGuid(), request.EmployeeId, request.time);
+            }
+
+            await unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Result.Ok();
+
+        }
+    }
+    #endregion
+}
--- Microservices/Hrms/QubeFin.Hrms.Persistence/Repositories/AttendanceRepository.cs
+++ Microservices/Hrms/QubeFin.Hrms.Persistence/Repositories/AttendanceRepository.cs
@@ -4,18 +4,35 @@ using QubeFin.Persistence;
 using QubeFin.Persistence.Mappers.App;
 using QubeFin.Persistence.Models.App;
 using QubeFin.Persistence.Models.Hrms;
+using System.Reflection.Metadata.Ecma335;
 
 namespace QubeFin.Hrms.Persistence.Repositories
 {
     public interface IAttendanceRepository
     {
+        Task CreateAttendance(Attendance attendance);
+        Task<Attendance> GetTodayAttendanceData(Guid EmployeeId);
     }
     public class AuthRepository(QubeFinDataContext context) : IAttendanceRepository
     {
         public Task CreateAttendance(Attendance attendance)
         {
-            context.TblAttendances.Add(attendance.ToEntity());
+            var employeeOrganizationUnit = context.TblEmployees.Include(m => m.OrganizationUnit).Single(m => m.Id == attendance.EmployeeId);
+
+            var newAttendance = Attendance.MarkCheckIn(Guid.NewGuid(), attendance.EmployeeId, attendance.ActualInTime, employeeOrganizationUnit.OrganizationUnitId, employeeOrganizationUnit.OrganizationUnit?.AttendanceInTime, employeeOrganizationUnit.OrganizationUnit?.AttendanceOutTime);
+
+            context.TblAttendances.Add(newAttendance.ToEntity());
             return Task.CompletedTask;
+        }
+        public async Task<Attendance> GetTodayAttendanceData(Guid EmployeeId)
+        {
+            var attendanceEntity = await context.TblAttendances.FirstOrDefaultAsync(m => m.EmployeeId == EmployeeId && m.AttendanceDate == DateOnly.FromDateTime(DateTime.Now));
+            if (attendanceEntity is null)
+            {
+                return null;
+            }
+
+            return attendanceEntity.ToDomain();
         }
     }
 }
