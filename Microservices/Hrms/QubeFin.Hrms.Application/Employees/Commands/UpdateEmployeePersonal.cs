3f3e0b2d5bb7ad1a52fc393c8374c5abcb07652b
ayan.mondal@qbent.in <ayan.mondal@qbent.in>
2026-07-13 18:05:18 +0530

    Employee Create, Get, Update Personal & Official Done

--- Microservices/Hrms/QubeFin.Hrms.Application/Employees/Commands/UpdateEmployeePersonal.cs
+++ Microservices/Hrms/QubeFin.Hrms.Application/Employees/Commands/UpdateEmployeePersonal.cs
@@ -1,30 +1,26 @@
 ﻿using FluentResults;
 using FluentValidation;
 using MediatR;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.EntityFrameworkCore;
 using QubeFin.Core.Results;
 using QubeFin.Hrms.Persistence.Repositories;
 using QubeFin.Persistence;
-using QubeFin.Persistence.Models.App;
 using QubeFin.Persistence.Models.Hrms;
-using System.Reflection;
 using System.Text.RegularExpressions;
 
 namespace QubeFin.Hrms.Application.Employees.Commands;
 
-
 #region --- COMMAND ---
-public record CreateEmployeeCommand(string Code, string? Salutation, string FirstName, string? MiddleName, string LastName, string? FatherName, string? MotherName, 
+public record UpdateEmployeePersonalCommand(
+    Guid Id, string? Salutation, string FirstName, string? MiddleName, string LastName, string? FatherName, string? MotherName,
     DateOnly DateOfBirth, string Gender, string Religion, string? Caste, string Nationality, string BloodGroup, string? DisabilityType, string? MaritalStatus,
-    Guid CreatedBy
-) : IRequest<Result<CreateEmployeeResponse>>;
+    Guid UserId
+    ) : IRequest<Result<UpdateEmployeePersonalResponse>>;
 #endregion
 
 #region --- VALIDATION ---
-public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+public class UpdateEmployeePersonalCommandValidator : AbstractValidator<UpdateEmployeePersonalCommand>
 {
-    public CreateEmployeeCommandValidator()
+    public UpdateEmployeePersonalCommandValidator()
     {
         RuleFor(x => x.FirstName)
             .Must(value => !string.IsNullOrWhiteSpace(value)
@@ -37,47 +33,36 @@ public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCo
             .NotEmpty()
             .Matches("^[A-Za-z]{3,30}$")
             .WithMessage("Last name must contain only letters and be between 3 and 30 characters long.");
-        RuleFor(x => x.Code)
-            .NotEmpty()
-            .Matches("^[0-9]{6}$")
-            .WithMessage("Code must contain only numbers (0–9) and 6 digits long.");
+
     }
 }
 #endregion
 
 #region --- RESPONSE ---
-public record CreateEmployeeResponse(bool Created);
+public record UpdateEmployeePersonalResponse(bool Created);
 #endregion
 
 #region --- HANDLER ---
-internal sealed class CreateEmployeeCommandHandler(IEmployeeRepository employeeRepository, IUnitOfWork unitOfWork, QubeFinDataContext context)
-    : IRequestHandler<CreateEmployeeCommand, Result<CreateEmployeeResponse>>
+internal sealed class UpdateEmployeePersonalCommandHandler(IEmployeeRepository employeeRepository, IUnitOfWork unitOfWork)
+    : IRequestHandler<UpdateEmployeePersonalCommand, Result<UpdateEmployeePersonalResponse>>
 {
-    public async Task<Result<CreateEmployeeResponse>> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+    public async Task<Result<UpdateEmployeePersonalResponse>> Handle(UpdateEmployeePersonalCommand request, CancellationToken cancellationToken)
     {
-        var existingEmployee = await context.TblEmployees.FirstOrDefaultAsync(m => m.Code == request.Code);
-        if (existingEmployee != null)
+        var employee = await employeeRepository.GetByIdAsync(request.Id);
+        if (employee == null)
         {
-            return new ValidationError("Employee already exist with same code.");
+            return new ValidationError("Employee does not exist with the given id.");
         }
 
-        var employee = Employee.Create(
-            Guid.NewGuid(),
-            request.Code,
+        employee.UpdatePersonalInfo(
             new PersonalInfo(request.Salutation, request.FirstName, request.MiddleName, request.LastName, request.FatherName, request.MotherName,
                 request.DateOfBirth, request.Gender, request.Religion, request.Caste, request.Nationality, request.BloodGroup, request.DisabilityType, request.MaritalStatus),
-            new OfficialInfo(),
-            new ContactInfo(),
-            new AddressInfo(),
-            new AddressInfo(),
-            new PayrollInfo(),
-            request.CreatedBy
+            request.UserId
             );
 
-        await employeeRepository.AddAsync(employee);
+        await employeeRepository.UpdateAsync(employee);
         await unitOfWork.SaveChangesAsync(cancellationToken);
-
-        return Result.Ok(new CreateEmployeeResponse(true));
+        return Result.Ok(new UpdateEmployeePersonalResponse(true));
     }
 }
 #endregion
--- Microservices/Hrms/QubeFin.Hrms.Persistence/Repositories/EmployeeRepository.cs
+++ Microservices/Hrms/QubeFin.Hrms.Persistence/Repositories/EmployeeRepository.cs
@@ -1,72 +1,67 @@
-﻿using Azure.Core;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using QubeFin.Core.Results;
+﻿using Microsoft.EntityFrameworkCore;
 using QubeFin.Persistence;
 using QubeFin.Persistence.Mappers.App;
 using QubeFin.Persistence.Mappers.Hrms;
-using QubeFin.Persistence.Models.App;
 using QubeFin.Persistence.Models.Hrms;
-using static QubeFin.Persistence.Models.Hrms.Employee;
 
-namespace QubeFin.Hrms.Persistence.Repositories
+namespace QubeFin.Hrms.Persistence.Repositories;
+
+public interface IEmployeeRepository
 {
-    public interface IEmployeeRepository
+    Task<Employee?> GetByIdAsync(Guid id);
+    Task AddAsync(Employee employee);
+    Task UpdateAsync(Employee employee);
+    //Task<Employee> GetById(Guid employeeId);
+    //Task<EmployeeOrganizationTiming?> GetEmployeeOrganization(Guid employeeId);
+}
+public class EmployeeRepository(QubeFinDataContext context) : IEmployeeRepository
+{
+    public async Task AddAsync(Employee employee)
     {
-        Task<Employee?> GetByIdAsync(Guid id);
-        Task CreateEmployeeAsync(Employee employee);
-        //void UpdateEmployee(Employee employee);
-        //Task<Employee> GetById(Guid employeeId);
-        //Task<EmployeeOrganizationTiming?> GetEmployeeOrganization(Guid employeeId);
+        await context.TblEmployees.AddAsync(employee.ToEntity());
     }
-    public class EmployeeRepository(QubeFinDataContext context) : IEmployeeRepository
+
+    public async Task UpdateAsync(Employee employee)
     {
-        public async Task CreateEmployeeAsync(Employee employee)
-        {
-            await context.TblEmployees.AddAsync(employee.ToEntity());
-        }
+        context.TblEmployees.Update(employee.ToEntity());
+    }
 
-        //public void UpdateEmployee(Employee employee)
-        //{
-        //    context.TblEmployees.Update(employee.ToEntity());
-        //}
+    //public async Task<EmployeeOrganizationTiming?> GetEmployeeOrganization(Guid employeeId)
+    //{
+    //    var employee = await context.TblEmployees.Include(x => x.OrganizationUnit).FirstOrDefaultAsync(x => x.Id == employeeId);
 
-        //public async Task<EmployeeOrganizationTiming?> GetEmployeeOrganization(Guid employeeId)
-        //{
-        //    var employee = await context.TblEmployees.Include(x => x.OrganizationUnit).FirstOrDefaultAsync(x => x.Id == employeeId);
+    //    if (employee == null)
+    //        return null;
 
-        //    if (employee == null)
-        //        return null;
+    //    return new EmployeeOrganizationTiming
+    //    {
+    //        AttendanceInTime = employee.OrganizationUnit?.AttendanceInTime,
+    //        AttendanceOutTime = employee.OrganizationUnit?.AttendanceOutTime,
+    //        OrganizationUnitId = employee.OrganizationUnitId
+    //    };
+    //}
+    //public async Task<Employee> GetById(Guid employeeId)
+    //{
+    //    var existingEmployee = await context.TblEmployees.FirstOrDefaultAsync(m => m.Id == employeeId);
+    //    if (existingEmployee == null)
+    //    {
+    //        return null;
+    //    }
 
-        //    return new EmployeeOrganizationTiming
-        //    {
-        //        AttendanceInTime = employee.OrganizationUnit?.AttendanceInTime,
-        //        AttendanceOutTime = employee.OrganizationUnit?.AttendanceOutTime,
-        //        OrganizationUnitId = employee.OrganizationUnitId
-        //    };
-        //}
-        //public async Task<Employee> GetById(Guid employeeId)
-        //{
-        //    var existingEmployee = await context.TblEmployees.FirstOrDefaultAsync(m => m.Id == employeeId);
-        //    if (existingEmployee == null)
-        //    {
-        //        return null;
-        //    }
+    //    return existingEmployee.ToDomain();
 
-        //    return existingEmployee.ToDomain();
+    //}
+    public async Task<Employee?> GetByIdAsync(Guid id)
+    {
+        var employeeEntity = await context.TblEmployees
+            .Include(x => x.TblEmployeeDesignations)
+            .Include(x => x.TblEmployeeQualifications)
+            .Include(x => x.TblEmployeeEmployments)
+            .Include(x => x.TblEmployeeDocuments)
+            .Include(x => x.TblEmployeeReferences)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Id == id);
 
-        //}
-        public async Task<Employee?> GetByIdAsync(Guid id)
-        {
-            var employeeEntity = await context.TblEmployees
-                .Include(x => x.TblEmployeeDesignations)
-                .Include(x => x.TblEmployeeQualifications)
-                .Include(x => x.TblEmployeeEmployments)
-                .Include(x => x.TblEmployeeDocuments)
-                .Include(x => x.TblEmployeeReferences)
-                .FirstOrDefaultAsync(x => x.Id == id);
-
-            return employeeEntity is null ? null : employeeEntity.ToDomain();
-        }
+        return employeeEntity is null ? null : employeeEntity.ToDomain();
     }
 }
