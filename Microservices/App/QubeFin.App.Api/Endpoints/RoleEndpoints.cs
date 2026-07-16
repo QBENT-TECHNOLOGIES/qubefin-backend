a7aec37ed07cf8d7f90cb9d773940aa47e39a14b
ayan.mondal@qbent.in <ayan.mondal@qbent.in>
2026-07-16 19:01:57 +0530

    Refresh Token Added

---
+++ Microservices/App/QubeFin.App.Api/Endpoints/RoleEndpoints.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using QubeFin.App.Application.Roles.Queries;
+using QubeFin.Core.Endpoint;
+
+namespace QubeFin.App.Api.Endpoints;
+
+public class RoleEndpoints : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("roles", async (ISender sender) =>
+        {
+            var roles = await sender.Send(new GetRolesQuery());
+            return Results.Ok(roles.Value);
+        })
+        //.RequireAuthorization("Permission:Users.View")
+        .WithSummary("Get All Roles");
+    }
+}
---
+++ Shared/QubeFin.Core/Results/ResultExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentResults;
+using Microsoft.AspNetCore.Http;
+
+namespace QubeFin.Core.Results;
+
+public static class ResultExtensions
+{
+    public static IResult ToHttpResult<T>(this Result<T> result)
+    {
+        if (result.IsSuccess)
+        {
+            return Microsoft.AspNetCore.Http.Results.Ok(result.Value);
+        }
+
+        var error = result.Errors[0];
+
+        return error switch
+        {
+            UnauthorizedError => Microsoft.AspNetCore.Http.Results.Unauthorized(),
+            ForbiddenError => Microsoft.AspNetCore.Http.Results.Forbid(),
+            RecordNotFoundError => Microsoft.AspNetCore.Http.Results.NotFound(error),
+            ValidationError => Microsoft.AspNetCore.Http.Results.BadRequest(error),
+            _ => Microsoft.AspNetCore.Http.Results.Problem(error.Message)
+        };
+    }
+}
