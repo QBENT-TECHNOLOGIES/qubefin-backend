56976ea401f783850d6be336ed53de6faf8e2b73
subhendu <subhendu.samanta@qbent.in>
2026-07-17 13:16:41 +0530

    survey list page API implemented.

---
+++ Microservices/Global/QubeFin.Global.Api/Endpoints/SurveyEndpoints.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using QubeFin.Core.Endpoint;
+using QubeFin.Global.Application.Survey.Models;
+using QubeFin.Global.Application.Survey.Queries;
+
+namespace QubeFin.Global.Api.Endpoints
+{
+    public class SurveyEndpoints : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("surveys", async (SurveySearchParam searchParam, ISender sender) =>
+            {
+                var searchResults = await sender.Send(new GetSurveyBySearchQuery(searchParam));
+                return Results.Ok(searchResults);
+            }).WithSummary("Search All Surveys");
+
+            //app.MapGet("surveys/{id:guid}", async (Guid id, ISender sender) =>
+            //{
+            //    var survey = await sender.Send(new GetBranchSurveyByIdQuery(id));
+            //    return Results.Ok(survey.Value);
+            //}).WithSummary("Get Branch Survey By Id");
+        }
+    }
+}
---
+++ Microservices/Global/QubeFin.Global.Application/Survey/Models/SurveySearchParam.cs
@@ -0,0 +1,14 @@
+﻿using QubeFin.Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QubeFin.Global.Application.Survey.Models
+{
+    public class SurveySearchParam : SearchParam
+    {
+        public string? SurveyType { get; set; }
+        public DateOnly? SurveyFrom { get; set; }
+        public DateOnly? SurveyTo { get; set; }
+    }
+}
