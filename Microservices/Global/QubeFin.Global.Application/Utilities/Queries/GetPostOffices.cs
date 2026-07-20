2e4d155aee8fcc4b97f7417618d90bb171080028
kiritee-M416 <kiritee.pramanik@qbent.in>
2026-07-20 13:05:13 +0530

    ok

---
+++ Microservices/Global/QubeFin.Global.Application/Utilities/Queries/GetPostOffices.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using QubeFin.Persistence;
+
+namespace QubeFin.Global.Application.OrganizationUnitTypes.Queries;
+
+#region --- QUERY ---
+public record GetPostOfficesQuery : IRequest<Result<List<GetPostOfficeResponse>>>;
+#endregion
+
+#region --- RESPONSE ---
+public record GetPostOfficeResponse(Guid Id, string Name);
+#endregion
+
+#region --- HANDLER ---
+internal sealed class GetPostOfficesQueryHandler(QubeFinDataContext context)
+    : IRequestHandler<GetPostOfficesQuery, Result<List<GetPostOfficeResponse>>>
+{
+    public async Task<Result<List<GetPostOfficeResponse>>> Handle(GetPostOfficesQuery request, CancellationToken cancellationToken)
+    {
+        var utilities = await context
+            .TblPostOffices
+            .OrderBy(a => a.Name)
+            .Select(a => new GetPostOfficeResponse(a.Id, a.Name))
+            .ToListAsync(cancellationToken);
+
+        return Result.Ok(utilities);
+    }
+}
+#endregion
