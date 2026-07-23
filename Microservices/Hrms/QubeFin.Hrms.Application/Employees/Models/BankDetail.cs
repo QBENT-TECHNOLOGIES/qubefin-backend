8453ac3c1a184bcecb27a9dfb3725b725ba98a69
kiritee-M416 <kiritee.pramanik@qbent.in>
2026-07-23 17:10:05 +0530

    ok

--- Microservices/Hrms/QubeFin.Hrms.Application/Employees/Models/BankDetail.cs
+++ Microservices/Hrms/QubeFin.Hrms.Application/Employees/Models/BankDetail.cs
@@ -2,11 +2,11 @@
 
 public class BankDetail
 {
-    public Guid BankId { get;  set; }
+    public Guid? BankId { get;  set; }
     public long BankAccountNo { get;  set; }
     public string BankHolderName { get;  set; }
-    public string BankBranch { get;  set; }
-    public string BankAccountType { get;  set; }
+    public string? BankBranch { get;  set; }
+    public string? BankAccountType { get;  set; }
     public bool HasEsiEligible { get;  set; }
     public string? EsiIpNumber { get;  set; }
     public string? UniversalAccountNumber { get;  set; }
