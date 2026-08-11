c4359430a63e33ab875841d1b85cca184538586c
Ritam Bera <ritam.bera@qbent.in>
2026-08-11 17:02:42 +0530

    Leave prayer List and create api added

---
+++ Microservices/Hrms/QubeFin.Hrms.Api/EndPoints/LeavePrayerEndpoints.cs
@@ -0,0 +1,59 @@
+﻿using Amazon.Auth.AccessControlPolicy;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using QubeFin.Core.Endpoint;
+using QubeFin.Core.Identity;
+using QubeFin.Core.Results;
+using QubeFin.Hrms.Application.Attendances.Models;
+using QubeFin.Hrms.Application.LeavePrayers.Commands;
+using QubeFin.Hrms.Application.LeavePrayers.Models;
+using QubeFin.Hrms.Application.LeavePrayers.Queries;
+using QubeFin.Hrms.Application.LeaveTypes.Queries;
+using System.Security.Claims;
+
+namespace QubeFin.Hrms.Api.EndPoints
+{
+    public class LeavePrayerEndpoints : IEndpoint
+    {
+        public void MapEndpoint(IEndpointRouteBuilder app)
+        {
+            app.MapPost("leave/prayers", async (ClaimsPrincipal principal, [FromForm] LeavePrayerRequest request, ISender sender) =>
+            {
+                if (principal.Identity is null)
+                {
+                    return Results.Forbid();
+                }
+                var empId = principal.Identity.GetEmployeeId();
+                var userId = principal.Identity.GetUserId();
+                var command = new ApplyLeavePrayerCommand(request, empId, userId);
+                var result = await sender.Send(command);
+                if (result.IsFailed)
+                {
+                    if (result.Errors[0] is QubeFin.Core.Results.RecordNotFoundError)
+                    {
+                        return Results.NotFound(result.Errors[0]);
+                    }
+                    if (result.Errors[0] is QubeFin.Core.Results.ValidationError)
+                    {
+                        return Results.BadRequest(result.Errors[0]);
+                    }
+                }
+                return Results.Ok(result.Value);
+            }).DisableAntiforgery().WithSummary("Apply Leave prayer.").WithTags("Leave Prayer");
+
+            app.MapGet("leave/prayers/by-year/{year}", async (ClaimsPrincipal principal, ISender sender, [FromRoute] int year, CancellationToken cancellationToken) =>
+            {
+                if (principal.Identity is null)
+                {
+                    return Results.Forbid();
+                }
+
+                var employeeId = principal.Identity.GetEmployeeId();
+                var result = await sender.Send(new GetPrayerByEmployeeIdQuery(year, employeeId));
+                return result.ToHttpResult();
+            })
+            .WithSummary("Get year wise Leave Payers for specific employee")
+            .WithTags("Leave Prayer");
+        }
+    }
+}
--- Microservices/Hrms/QubeFin.Hrms.Application/LeavePrayers/Commands/ApplyLeavePrayer.cs
+++ Microservices/Hrms/QubeFin.Hrms.Application/LeavePrayers/Commands/ApplyLeavePrayer.cs
@@ -1,10 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentResults;
+using FluentValidation;
+using MediatR;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using QubeFin.Hrms.Application.LeavePrayers.Models;
+using QubeFin.Hrms.Persistence.Repositories;
+using QubeFin.Persistence;
+using System.Data;
 
-namespace QubeFin.Hrms.Application.LeavePrayers.Commands
+namespace QubeFin.Hrms.Application.LeavePrayers.Commands;
+
+#region --- COMMAND ---
+public record ApplyLeavePrayerCommand(LeavePrayerRequest prayer, Guid EmployeeId, Guid UserId) : IRequest<Result<ApplyLeavePrayerResponse>>;
+#endregion
+
+#region --- VALIDATOR ---
+public class ApplyLeavePrayerCommandValidator : AbstractValidator<ApplyLeavePrayerCommand>
 {
-    internal class ApplyLeavePrayer
+    public ApplyLeavePrayerCommandValidator()
     {
+        RuleFor(v => v.prayer).NotNull().WithMessage("Regularization request is required.");
+        RuleFor(v => v.EmployeeId).NotEqual(Guid.Empty).WithMessage("Employee Id is required.");
     }
 }
+#endregion
+
+#region --- RESPONSE ---
+public record ApplyLeavePrayerResponse(bool success, string message);
+#endregion
+
+#region --- HANDLER ---
+internal sealed class ApplyLeavePrayerCommandHandler(QubeFinDataContext context, IUnitOfWork unitOfWork, IFileStorageRepository fileStorageRepository) : IRequestHandler<ApplyLeavePrayerCommand, Result<ApplyLeavePrayerResponse>>
+{
+    public async Task<Result<ApplyLeavePrayerResponse>> Handle(ApplyLeavePrayerCommand request, CancellationToken cancellationToken)
+    {
+        string? attachment = null;
+        if (request.prayer.Attachment != null && request.prayer.Attachment.Length > 0)
+        {
+            try
+            {
+                var file = request.prayer.Attachment;
+                await using var stream = file.OpenReadStream();
+                attachment = await fileStorageRepository.UploadFileAsync(stream, file.FileName, file.ContentType ?? "application/octet-stream", cancellationToken).ConfigureAwait(false);
+            }
+            catch
+            {
+                attachment = null;
+            }
+        }
+        var successParam = new SqlParameter("@Success", SqlDbType.Bit)
+        {
+            Direction = ParameterDirection.Output
+        };
+        var messageParam = new SqlParameter("@Message", SqlDbType.NVarChar, 255)
+        {
+            Direction = ParameterDirection.Output
+        };
+        var regularizationIdParam = new SqlParameter("@RegularizationId", SqlDbType.UniqueIdentifier)
+        {
+            Direction = ParameterDirection.Output
+        };
+
+        await context.Database.ExecuteSqlRawAsync(
+            @"EXEC [Hrms].[USP_RegularizationApplied]
+                @Id,
+                @EmployeeId,
+                @UserId,
+                @LeaveTypeId ,
+                @PrayerDays,
+                @Attachment,
+                @Remarks
+                @Success OUTPUT,
+                @Message OUTPUT,
+                @PrayerId OUTPUT",
+        new SqlParameter("@Id", request.prayer.Id),
+        new SqlParameter("@EmployeeId", request.EmployeeId),
+        new SqlParameter("@UserId", request.UserId),
+        new SqlParameter("@LeaveTypeId", request.prayer.LeaveTypeId),
+        new SqlParameter("@PrayerDays", request.prayer.PrayerDays),
+        new SqlParameter("@Attachment", (object?)attachment ?? DBNull.Value),
+        new SqlParameter("@Remarks", (object?)request.prayer.Remarks ?? DBNull.Value),
+        successParam,
+        messageParam,
+        regularizationIdParam);
+
+        bool success = successParam.Value != DBNull.Value && (bool)successParam.Value;
+        string message = messageParam.Value?.ToString() ?? string.Empty;
+        Guid? regularizationId = regularizationIdParam.Value == DBNull.Value ? null : (Guid)regularizationIdParam.Value;
+
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return Result.Ok(new ApplyLeavePrayerResponse(success, $"{message}"));
+    }
+}
+#endregion
