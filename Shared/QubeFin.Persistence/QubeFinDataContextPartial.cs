78e9303d7e52014e4b6019ac4432a43158068c0f
Ritam Bera <ritam.bera@qbent.in>
2026-08-11 18:35:29 +0530

    small change

--- Shared/QubeFin.Persistence/QubeFinDataContextPartial.cs
+++ Shared/QubeFin.Persistence/QubeFinDataContextPartial.cs
@@ -24,6 +24,7 @@ public partial class QubeFinDataContext : DbContext, IUnitOfWork
         modelBuilder.Entity<LeaveRequestResponse>().HasNoKey().ToView(null);
         modelBuilder.Entity<LeaveApprovalSearchResult>().HasNoKey().ToView(null);
         modelBuilder.Entity<ApprovalWorkflowEventGroupItem>().HasNoKey().ToView(null);
+        modelBuilder.Entity<LeavePrayerResponse>().HasNoKey().ToView(null);
     }
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
