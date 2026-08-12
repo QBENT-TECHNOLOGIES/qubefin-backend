8306d1c8e3484e3dd1dbbb2fe16292f480092441
subhendu <subhendu.samanta@qbent.in>
2026-08-12 15:29:54 +0530

    Notification all marked as read api done

--- Microservices/Global/QubeFin.Global.Api/Endpoints/NotificationEndpoints.cs
+++ Microservices/Global/QubeFin.Global.Api/Endpoints/NotificationEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using QubeFin.Core.Endpoint;
 using QubeFin.Core.Identity;
+using QubeFin.Global.Application.Notifications.Commands;
 using QubeFin.Global.Application.Notifications.Queries;
 using System.Security.Claims;
 
@@ -25,6 +26,34 @@ namespace QubeFin.Global.Api.Endpoints
             .WithTags("Notifications")
             .Produces(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status500InternalServerError);
+
+            app.MapGet("notifications/{notificationId:guid}/read", async (ISender sender, ClaimsPrincipal principal, Guid notificationId, CancellationToken cancellationToken) =>
+            {
+                var employeeId = principal.Identity.GetEmployeeId();
+                var result = await sender.Send(new ReadNotificationCommand(notificationId), cancellationToken);
+                if (result.IsFailed)
+                {
+                    return Results.Problem("Failed to mark notification as read");
+                }
+                return Results.Ok(result.Value);
+            }).WithName("MarkAsRead")
+            .WithSummary("Mark Notification as Read")
+            .WithDescription("Marks a specific notification as read for the authenticated user.")
+            .WithTags("Notifications");
+
+            app.MapGet("notifications/read-all", async (ISender sender, ClaimsPrincipal principal, CancellationToken cancellationToken) =>
+            {
+                var employeeId = principal.Identity.GetEmployeeId();
+                var result = await sender.Send(new ReadAllNotificationCommand(employeeId), cancellationToken);
+                if (result.IsFailed)
+                {
+                    return Results.Problem("Failed to mark notification as read");
+                }
+                return Results.Ok(result.Value);
+            }).WithName("MarkAllAsRead")
+            .WithSummary("Mark All Notifications as Read")
+            .WithDescription("Marks all notifications as read for the authenticated user.")
+            .WithTags("Notifications");
         }
     }
 }
---
+++ Microservices/Global/QubeFin.Global.Application/Notifications/Commands/ReadNotification.cs
@@ -0,0 +1,26 @@
+﻿using FluentResults;
+using MediatR;
+using QubeFin.Global.Persistence.Repositories;
+using QubeFin.Persistence;
+
+namespace QubeFin.Global.Application.Notifications.Commands;
+
+#region --- COMMAND ---
+public record ReadNotificationCommand(Guid Id) : IRequest<Result<ReadNotificationResponse>>;
+#endregion
+
+#region --- RESPONSE ---
+public record ReadNotificationResponse(bool Created);
+#endregion
+
+#region --- HANDLER ---
+internal sealed class ReadNotificationCommandHandler(INotificationRepository notificationRepository, IUnitOfWork unitOfWork) : IRequestHandler<ReadNotificationCommand, Result<ReadNotificationResponse>>
+{
+    public async Task<Result<ReadNotificationResponse>> Handle(ReadNotificationCommand request, CancellationToken cancellationToken)
+    {
+        var success = await notificationRepository.MarkAsReadAsync(request.Id);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+        return Result.Ok(new ReadNotificationResponse(true));
+    }
+}
+#endregion
