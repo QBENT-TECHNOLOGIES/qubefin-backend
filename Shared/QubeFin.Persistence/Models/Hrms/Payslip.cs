fc619341e6cdb2a97babf3147ee3aa4274e56ad7
subhendu <subhendu.samanta@qbent.in>
2026-08-18 11:36:01 +0530

    rename column and make month and year both concatinate.

--- Shared/QubeFin.Persistence/Models/Hrms/Payslip.cs
+++ Shared/QubeFin.Persistence/Models/Hrms/Payslip.cs
@@ -12,8 +12,7 @@ namespace QubeFin.Persistence.Models.Hrms
         public string? Designation { get; set; } = string.Empty;
         public string? OrganizationUnitName { get; set; } = string.Empty;
         public string? SalaryGrade { get; set; } = string.Empty;
-        public int? PayrollMonth { get; set; }
-        public int? PayrollYear { get; set; }
+        public string? PayrollMonthYear { get; set; }
         public decimal? TotalEarning { get; set; }
         public decimal? TotalDeduction { get; set; }
         public decimal? NetPay { get; set; }
