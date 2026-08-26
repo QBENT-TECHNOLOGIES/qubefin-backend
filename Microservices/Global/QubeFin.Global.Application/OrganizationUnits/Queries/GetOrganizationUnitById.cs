085f85435a77ee2e0cd2251a4ec35b50c3d6dc85
subhendu <subhendu.samanta@qbent.in>
2026-08-26 14:42:33 +0530

    Add Designations into Organization unit GET API

--- Microservices/Global/QubeFin.Global.Application/OrganizationUnits/Queries/GetOrganizationUnitById.cs
+++ Microservices/Global/QubeFin.Global.Application/OrganizationUnits/Queries/GetOrganizationUnitById.cs
@@ -13,7 +13,8 @@ public record GetOrganizationUnitByIdQuery(Guid Id) : IRequest<Result<GetOrganiz
 
 #region --- RESPONSE ---
 public record GetOrganizationUnitByIdResponse(Guid Id, string Name, Guid OrganizationUnitTypeId, string OrganizationUnitTypeIcon, string OrganizationUnitTypeName, Guid? ParentId,
-    string CreatedBy, DateTime CreatedOn, string? LastModifiedBy, DateTime? LastModifiedOn, IReadOnlyList<OrganizationUnitHierarchyItem> Hierarchy);
+    string CreatedBy, DateTime CreatedOn, string? LastModifiedBy, DateTime? LastModifiedOn, IReadOnlyList<OrganizationUnitHierarchyItem> Hierarchy,
+    IReadOnlyList<OragnizationDesignations> Designations);
 #endregion
 
 #region --- HANDLER ---
@@ -22,6 +23,15 @@ internal sealed class GetOrganizationUnitByIdQueryHandler(QubeFinDataContext con
 {
     public async Task<Result<GetOrganizationUnitByIdResponse>> Handle(GetOrganizationUnitByIdQuery request, CancellationToken cancellationToken)
     {
+        var organizationUnit = await context
+         .TblOrganizationUnits
+         .Include(m => m.OrganizationUnitType)
+         .Where(m => m.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
+        if (organizationUnit is null)
+        {
+            return new RecordNotFoundError($"Organization Unit not found for the given Id");
+        }
+
         var hierarchy = await context.Set<OrganizationUnitHierarchyItem>()
             .FromSqlInterpolated($@"
                 ;WITH Hierarchy AS
@@ -60,19 +70,31 @@ internal sealed class GetOrganizationUnitByIdQueryHandler(QubeFinDataContext con
             .AsNoTracking()
             .ToListAsync(cancellationToken);
 
-        var organizationUnit = await context
-            .TblOrganizationUnits
-            .Include(m => m.OrganizationUnitType)
-            .Where(m => m.Id == request.Id)
-            .Select(m => new GetOrganizationUnitByIdResponse(m.Id, m.Name, m.OrganizationUnitTypeId, m.OrganizationUnitType.Icon, m.OrganizationUnitType.Name, m.ParentId,
-                m.CreatedByNavigation.UserName, m.CreatedOn, m.LastModifiedByNavigation.UserName, m.LastModifiedOn, hierarchy))
-            .FirstOrDefaultAsync(cancellationToken);
+        var designations = await context.TblDesignations
+            .Where(d => d.OrganizationUnitId == request.Id)
+            .Select(d => new OragnizationDesignations
+            {
+                Id = d.Id,
+                Name = d.Name,
+                PostId = d.PostId,
+                PostName = d.Post.Name,
+                IsActive = d.IsActive,
+                RoleId = d.TblDesignationRoles.Select(r => (Guid?)r.RoleId).FirstOrDefault(),
+                RoleName = d.TblDesignationRoles.Select(r => r.Role.Name).FirstOrDefault(),
+                GradeId = d.TblDesignationGradeMappings.Select(g => (Guid?)g.GradeId).FirstOrDefault(),
+                GradeName = d.TblDesignationGradeMappings.Select(g => g.Grade.Name).FirstOrDefault(),
+            })
+            .AsNoTracking()
+            .ToListAsync(cancellationToken);
 
-        if (organizationUnit is null)
-        {
-            return new RecordNotFoundError($"Organization Unit not found for the given Id");
-        }
-        return Result.Ok(organizationUnit);
+        var result = new GetOrganizationUnitByIdResponse(
+            organizationUnit.Id, organizationUnit.Name,
+            organizationUnit.OrganizationUnitTypeId, organizationUnit.OrganizationUnitType.Icon,
+            organizationUnit.OrganizationUnitType.Name, organizationUnit.ParentId,
+            organizationUnit.CreatedByNavigation.UserName, organizationUnit.CreatedOn,
+            organizationUnit.LastModifiedByNavigation?.UserName, organizationUnit.LastModifiedOn, hierarchy, designations);
+
+        return Result.Ok(result);
     }
 }
 #endregion
