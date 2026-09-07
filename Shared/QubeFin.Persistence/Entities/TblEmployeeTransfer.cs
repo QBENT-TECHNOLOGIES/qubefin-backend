7a4428c9aabafb9c48003227315861cacdbe9e5b
Ritam Bera <ritam.bera@qbent.in>
2026-09-07 18:43:41 +0530

    DB Updated

---
+++ Shared/QubeFin.Persistence/Entities/TblEmployeeTransfer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QubeFin.Persistence.Entities;
+
+public partial class TblEmployeeTransfer
+{
+    public Guid Id { get; set; }
+
+    public Guid EmployeeId { get; set; }
+
+    public Guid OrganisationUnitId { get; set; }
+
+    public Guid DesignationId { get; set; }
+
+    public Guid SalaryGradeId { get; set; }
+
+    public decimal GrossSalary { get; set; }
+
+    public DateOnly FromDate { get; set; }
+
+    public DateOnly? ToDate { get; set; }
+
+    public bool IsApprove { get; set; }
+
+    public virtual TblDesignation Designation { get; set; } = null!;
+
+    public virtual TblEmployee Employee { get; set; } = null!;
+
+    public virtual TblOrganizationUnit OrganisationUnit { get; set; } = null!;
+
+    public virtual TblSalaryGrade SalaryGrade { get; set; } = null!;
+}
---
+++ Shared/QubeFin.Persistence/Entities/TblTempBranch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QubeFin.Persistence.Entities;
+
+public partial class TblTempBranch
+{
+    public string? Name { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? GoogleAddress { get; set; }
+
+    public string? Lat { get; set; }
+
+    public string? Lon { get; set; }
+}
