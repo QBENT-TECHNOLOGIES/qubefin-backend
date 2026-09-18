6aa2ef36a19820a41a9fedfa4070ee3846a8fa6d
Sujan Ghosh <sujan.ghosh@qbent.in>
2026-09-18 17:06:35 +0530

    changes

--- Microservices/Report/QubeFin.Report.Api/Endpoints/HrmsReportEndpoints.cs
+++ Microservices/Report/QubeFin.Report.Api/Endpoints/HrmsReportEndpoints.cs
@@ -241,6 +241,44 @@ namespace QubeFin.Report.Api.Endpoints
                 return Results.File(file.FileStream, file.ContentType, file.FileName);
             }).WithSummary("Generate Wegro BC interview panel acknowledgement.");
 
+            app.MapGet("/interview/wegrow-jobapplication-form/{candidateId:guid}", [Authorize] async (Guid candidateId, ISender sender) =>
+            {
+                var command = new GenerateSSRSReportsCommand(
+                "Rpt_Wegrow_JobApplication",
+                "PDF",
+                new Dictionary<string, string>
+                {
+                    ["CandidateId"] = candidateId.ToString()
+                });
+
+                var result = await sender.Send(command);
+
+                if (result.IsFailed)
+                    return result.ToHttpResult();
+
+                var file = result.Value;
+                return Results.File(file.FileStream, file.ContentType, file.FileName);
+            }).WithSummary("Generate Wegrow job application form.");
+
+            app.MapGet("/interview/weegrobc-jobapplication-form/{candidateId:guid}", [Authorize] async (Guid candidateId, ISender sender) =>
+            {
+                var command = new GenerateSSRSReportsCommand(
+                "Rpt_WeegroBC_JobApplication",
+                "PDF",
+                new Dictionary<string, string>
+                {
+                    ["CandidateId"] = candidateId.ToString()
+                });
+
+                var result = await sender.Send(command);
+
+                if (result.IsFailed)
+                    return result.ToHttpResult();
+
+                var file = result.Value;
+                return Results.File(file.FileStream, file.ContentType, file.FileName);
+            }).WithSummary("Generate Wegro BC job application form.");
+
             #endregion
         }
     }
